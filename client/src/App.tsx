78856a7448a2e0d9fffd93a6dc793f0116a95713
Javaid Baksh <javaidbaksh@gmail.com>
2025-12-18 19:34:46 -0500

    Add NewsBoard route and remove Trends route from App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,13 +5,13 @@ import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import Breakdown from './pages/Breakdown';
 import Watchlist from './pages/Watchlist';
-import Trends from './pages/Trends';
 import Analysis from './pages/Analysis';
 import Ratios from './pages/Ratios';
 import Portfolio from './pages/Portfolio';
 import Search from './pages/Search';
 import CacheManagement from './pages/CacheManagement';
 import PnLTracker from './pages/PnLTracker';
+import NewsBoard from './components/NewsBoard';
 import Icons from './components/Icons';
 import NotFound from './pages/NotFound';
 import IconTest from './components/IconTest';
@@ -25,11 +25,11 @@ function App() {
             <Route path="/" element={<Breakdown />} />
             <Route path="/breakdown" element={<Breakdown />} />
             <Route path="/watchlist" element={<Watchlist />} />
-            <Route path="/trends" element={<Trends />} />
             <Route path="/analysis" element={<Analysis />} />
             <Route path="/ratios" element={<Ratios />} />
             <Route path="/portfolio" element={<Portfolio />} />
             <Route path="/pnl" element={<PnLTracker />} />
+            <Route path="/newsboard" element={<NewsBoard />} />
             <Route path="/search" element={<Search />} />
             <Route path="/icons" element={<Icons />} />
             <Route path="/cache" element={<CacheManagement />} />
