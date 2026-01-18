ffa40c9e2a9d30c2b20cd526e05a1eaf7c074055
Javaid Baksh <javaidbaksh@gmail.com>
2026-01-18 16:11:47 -0500

    Update weeklyChangePercent type to allow array of numbers

--- client/src/components/PortfolioAllocationPieChart.tsx
+++ client/src/components/PortfolioAllocationPieChart.tsx
@@ -19,7 +19,7 @@ interface Holding {
   unrealizedPnL?: number;
   totalPnL?: number;
   totalPnLPercent?: number;
-  weeklyChangePercent?: number | null;
+  weeklyChangePercent?: number | number[] | null;
   usdPrice?: number;
   exchangeRate?: number;
 }
